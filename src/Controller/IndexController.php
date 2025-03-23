91cb83e6f5f11f841c844c3cf7615dcb64c6c760
lyradix <daryl.stpaul@gmail.com>
2025-03-24 00:33:09 +0100

    json pages

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\ArtistRepository;
+use App\Repository\InfoRepository;
+use App\Repository\PartnersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +27,22 @@ final class IndexController extends AbstractController
     {
         $data = $ArtistRepository->findAllWithScenes();
         $jsonData = $serializer->serialize($data, 'json', ['groups' => ['artist:read', 'scene:read']]);
+        return new JsonResponse($jsonData, 200, [], true);
+    }
+
+    #[Route('/partners', name: 'app_partners')]
+    public function getPartners(PartnersRepository $PartnersRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $data = $PartnersRepository->findAll();
+        $jsonData = $serializer->serialize($data, 'json', ['partners:read']);
+        return new JsonResponse($jsonData, 200, [], true);
+    }
+
+    #[Route('/info', name: 'app_info')]
+    public function getInfo(InfoRepository $InfoRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $data = $InfoRepository->findAll();
+        $jsonData = $serializer->serialize($data, 'json', ['info:read']);
         return new JsonResponse($jsonData, 200, [], true);
     }
 }
