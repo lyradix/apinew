14fdf78d6d90f8ae8de6c2a49b950fcf43af4494
lyradix <daryl.stpaul@gmail.com>
2025-07-17 18:07:46 +0200

    new field added in modifcation form for artist for adding image

--- src/Form/ModifConcertType.php
+++ src/Form/ModifConcertType.php
@@ -8,6 +8,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -45,6 +47,18 @@ class ModifConcertType extends AbstractType
                 'attr' => [
                     'class' => 'form-select',
                 ],
+            ])
+           ->add('image', FileType::class, [
+                'label' => 'Ajouter image',
+                'required' => false,
+                'mapped' => false, //set to false if image is not directly mapped to entity
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/webb'],
+                        'mimeTypesMessage' => 'Merci de télécharger une image'
+                    ])   
+                    ],
             ])
         ;
     }
